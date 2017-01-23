b6aecd870a4934f5848bf9defb60530289d4bc81
Vegard Antun <vegarant@student.matnat.uio.no>
2017-01-23 08:48:38 +0100

    Made string search case insensitive

--- matlab/findDWTKernel.m
+++ matlab/findDWTKernel.m
@@ -1,28 +1,28 @@
 function f= findDWTKernel(wave_name)
     % Find the DWTKernel corresponding to the given wavelet name 
-    if strcmp(wave_name, 'cdf97')
+    if strcmpi(wave_name, 'cdf97')
         f = @(x, symm, dual) DWTKernel97(x, symm, dual);
-    elseif strcmp(wave_name, 'cdf53')
+    elseif strcmpi(wave_name, 'cdf53')
         f = @(x, symm, dual) DWTKernel53(x, symm, dual);
-    elseif strcmp(wave_name, 'pwl0')
+    elseif strcmpi(wave_name, 'pwl0')
         f = @(x, symm, dual) DWTKernelpwl0(x, symm, dual);
-    elseif strcmp(wave_name, 'pwl2')
+    elseif strcmpi(wave_name, 'pwl2')
         f = @(x, symm, dual) DWTKernelpwl2(x, symm, dual);
-    elseif strcmp(wave_name, 'Haar')
+    elseif strcmpi(wave_name, 'Haar')
         f = @(x, symm, dual) DWTKernelHaar(x, symm, dual);
-    elseif (strcmp(wave_name(1:2), 'db') && ~strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:2), 'db') && ~strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(3:end));
         filters = liftingfactortho(vm, 0, 0);
         f = @(x, symm, dual) DWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:2), 'db') && strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:2), 'db') && strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(3:end-1));
         filters = liftingfactortho(vm, 0, 1);
         f = @(x, symm, dual) DWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:3), 'sym') && ~strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:3), 'sym') && ~strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(4:end));
         filters = liftingfactortho(vm, 1, 0);
         f = @(x, symm, dual) DWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:3), 'sym') && strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:3), 'sym') && strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(4:end-1));
         filters = liftingfactortho(vm, 1, 1);
         f = @(x, symm, dual) DWTKernelOrtho(x, filters, symm, dual);
--- matlab/findIDWTKernel.m
+++ matlab/findIDWTKernel.m
@@ -1,28 +1,28 @@
 function f = findIDWTKernel(wave_name)
     % Find the IDWTKernel corresponding to the given wavelet name 
-    if strcmp(wave_name, 'cdf97')
+    if strcmpi(wave_name, 'cdf97')
         f = @(x, symm, dual) IDWTKernel97(x, symm, dual);
-    elseif strcmp(wave_name, 'cdf53')
+    elseif strcmpi(wave_name, 'cdf53')
         f = @(x, symm, dual) IDWTKernel53(x, symm, dual);
-    elseif strcmp(wave_name, 'pwl0')
+    elseif strcmpi(wave_name, 'pwl0')
         f = @(x, symm, dual) IDWTKernelpwl0(x, symm, dual);
-    elseif strcmp(wave_name, 'pwl2')
+    elseif strcmpi(wave_name, 'pwl2')
         f = @(x, symm, dual) IDWTKernelpwl2(x, symm, dual);
-    elseif strcmp(wave_name, 'Haar')
+    elseif strcmpi(wave_name, 'Haar')
         f = @(x, symm, dual) IDWTKernelHaar(x, symm, dual);
-    elseif (strcmp(wave_name(1:2), 'db') && ~strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:2), 'db') && ~strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(3:end));
         filters = liftingfactortho(vm, 0, 0);
         f = @(x, symm, dual) IDWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:2), 'db') && strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:2), 'db') && strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(3:end-1));
         filters = liftingfactortho(vm, 0, 1);
         f = @(x, symm, dual) IDWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:3), 'sym') && ~strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:3), 'sym') && ~strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(4:end));
         filters = liftingfactortho(vm, 1, 0);
         f = @(x, symm, dual) IDWTKernelOrtho(x, filters, symm, dual);
-    elseif (strcmp(wave_name(1:3), 'sym') && strcmp(wave_name(end), 'x'))
+    elseif (strcmpi(wave_name(1:3), 'sym') && strcmpi(wave_name(end), 'x'))
         vm = str2double(wave_name(4:end-1));
         filters = liftingfactortho(vm, 1, 1);
         f = @(x, symm, dual) IDWTKernelOrtho(x, filters, symm, dual);
